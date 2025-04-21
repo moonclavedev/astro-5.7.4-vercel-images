42afcb64ae4657ae961cc1a3077e66d1eb2aec4c
Joe <98909677+moonclavedev@users.noreply.github.com>
2025-04-21 12:43:12 -0700

    Enable imageService & imagesConfig for Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,12 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel()
+  adapter: vercel({
+    imageService: true,
+    imagesConfig: {
+      domains: [],
+      sizes: [320, 640, 1280],
+      formats: ["image/avif", "image/webp"],
+    },
+  })
 });
